b75da9e23a5ba09ded19b4bbf6ee7a0dae697b74
Serena Wang (Chinasoft International) <v-sew@microsoft.com>
2020-05-19 10:07:08 +0000

    add source

--- src/VSConsoleCore/Program.cs
+++ src/VSConsoleCore/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,12 +10,41 @@ namespace VSOConsoleCore
         static void Main(string[] args)
         {
             VerifyValue();
-            sum(5,  6);
+            sum(5, 6);
+            VerifyVisualize();
             Foo().Wait();
             InvokeFunc(); //set breakpoint1
             VerifyException();
             Console.WriteLine("Hello World!");
             Console.ReadKey();
+        }
+
+        private static void VerifyVisualize()
+        {
+            string ht = "<html><body>hello world!</body><html>";
+            string xml2 = "<?xml version=\"1.0\" encoding=\"utf-8\" ?>\r\n<configuration>\r\n    <startup> \r\n        <supportedRuntime version=\"v4.0\" sku=\".NETFramework,Version=v4.6.1\" />\r\n    </startup>\r\n</configuration>";
+            string json = "{\r\n    \"name\":\"中国\",\r\n    \"province\":[\r\n    {\r\n       \"名字\":\"黑龙江\",\r\n        \"城市\":{\r\n            \"城市\":[\"海冰\",\"长春\"]\r\n        }\r\n     },\r\n      {\r\n        \"名字\": \"广东\",\r\n        \"城市\": {\r\n          \"城市\": [ \"广州\", \"深圳\", \"厦门\" ]\r\n        }\r\n      },\r\n    {\r\n        \"名字\":\"陕西\",\r\n      \"城市\": {\r\n        \"城市\": [ \"西安\", \"延安\" ]\r\n      }\r\n    },\r\n    {\r\n        \"名字\":\"甘肃\",\r\n      \"城市\": {\r\n        \"城市\": [ \"兰州\" ]\r\n      }\r\n    }\r\n]\r\n}\r\n";
+
+            DataTable dt = new DataTable("Table_AX");
+            dt.Columns.Add("column0", System.Type.GetType("System.String"));
+            DataColumn dc = new DataColumn("column1", System.Type.GetType("System.Boolean"));
+            dt.Columns.Add("column2", System.Type.GetType("System.Int32"));
+            dt.Columns.Add("column3", System.Type.GetType("System.Guid"));
+            dt.Columns.Add(dc);
+            Guid guid = new Guid("00000000-0000-0000-0000-000000000000");
+            for (int i = 0; i < 50; i++)
+            {
+                DataRow dr = dt.NewRow();
+                dr["column0"] = "AX_" + i;
+                dr["column1"] = true;
+                dr["column2"] = i;
+                dr["column3"] = guid;
+                dt.Rows.Add(dr);
+            }
+            DataRow dr1 = dt.NewRow();
+            dt.Rows.Add(dr1);
+            int a = 10;  //add a breakpoint here
+
         }
 
         private static void VerifyValue()
@@ -35,7 +65,7 @@ namespace VSOConsoleCore
 
         private static void VerifyException()
         {
-            
+
             try
             {
                 string s4 = null;
@@ -47,7 +77,7 @@ namespace VSOConsoleCore
             }
         }
 
-        
+
         static async Task Foo()
         {
             await GenException();
@@ -62,7 +92,7 @@ namespace VSOConsoleCore
         }
         private static int sum(int v1, int v2)
         {
-            return v1  +  v2;
+            return v1 + v2;
         }
 
         static async void InvokeFunc()
